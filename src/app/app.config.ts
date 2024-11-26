289e020a3b381d019df62af0dacb9379bdd052e1
carlosllana.tv <hola@carlosllana.tv>
2024-11-26 01:15:28 -0300

    angular 19

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter, withViewTransitions } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import {  provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { SpinnerInterceptor } from './shared/spinner.interceptor';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
